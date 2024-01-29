b3b497200d4216f0cf7a96c8fba151f4cc66af3a
robertastira <stira.roberta@yahoo.it>
2024-01-29 16:38:10 +0100

    Commit1

---
+++ src/button.jsx
@@ -0,0 +1,10 @@
+const ButtonComponent = function () {
+    return (
+        <div>
+          {/* un componente React può contenere solamente UN elemento JSX "padre" */}
+          <button>Bottone</button>
+        </div>
+    )
+  }
+  
+  export default ButtonComponent
